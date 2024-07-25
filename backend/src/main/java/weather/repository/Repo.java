088b5eed221a1211643bf01acc4d9c2cd4e51862
sunless13 <sunlesskelv@gmail.com>
2024-07-25 07:20:09 +0300

    Revert "feat(app): setup api"
    This reverts commit 42ceec3449d8cde295de4a630f9958d50305e1cf.

--- backend/src/main/java/weather/repository/Repo.java
+++
@@ -1,7 +0,0 @@
-package weather.repository;
-
-import weather.entity.Ent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repo extends JpaRepository<Ent, Long> {
-}
